372045181b7fbfb9618781765fc1f5324f47c1e5
dt46 <prezaa971@gmail.com>
2024-05-25 01:24:41 +0700

    migration v.2

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cart extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array<int, string>
+    */
+    protected $fillable = [
+        'id_reseller',
+        'id_produk',
+        'jumlah_produk',
+        'total_harga',
+        'deleted_at'
+    ];
+
+    public function reseller(): BelongsTo
+    {
+        return $this->belongsTo(Reseller::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- database/migrations/2024_05_13_164023_create_orders_table.php
+++ database/migrations/2024_05_13_164023_create_orders_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->uuid('id')->unique();
-            $table->foreignUuid('id_cart')->unique();
+            $table->foreignUuid('id_reseller');
             $table->string('metode_pembayaran');
             $table->integer('biaya_layananAplikasi');
             $table->integer('ongkos_kirim');
             $table->string('bukti_pembayaran');
             $table->string('nama_file_original');
+            $table->string('no_resi')->nullable();
+            $table->string('status')->default('verifikasi');
+            $table->timestamps('tanggal');
             $table->timestamps();
         });
     }
